bf5a6d7accd3d271e5014ac8babed709dd067946
Andrew Vedady <drewvedady@gmail.com>
2016-04-04 15:20:33 -0500

    basic basic server

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 // var express = require('express');
 // var partials = require('express-partials');
 // var app = express();
-
-
 var http = require('http');
 
 
-var port = process.env.PORT || 8090;
 
-function handleRequest(req, res) {
-  response.end('It Works. Path is : ' + req.url);
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(port, function() {
-  console.log('Server is listening on port ' + port);
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
 });
+
+server.listen(7000);
 
