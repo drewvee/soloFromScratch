610730d1bfdc47ce941c77c3b7972997de6db063
Andrew Vedady <drewvedady@gmail.com>
2016-04-04 15:25:00 -0500

    debugging

--- server.js
+++ server.js
@@ -4,11 +4,12 @@
 var http = require('http');
 
 
+var port = process.env.PORT || 8090;
 
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello World\n");
 });
 
-server.listen(7000);
+server.listen(port);
 
