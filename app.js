a49712c38609e425718219249e3ad7a0c36c844b
Andrew Vedady <drewvedady@gmail.com>
2016-04-04 14:31:55 -0500

    adding dependencies

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("It's a start");
+var express = require('express');
+var partials = require('express-partials');
+
+var app = express();
+
+var port = process.env.PORT || 8090;
+
+app.listen(port);
+
+
+console.log('Server is listening on port' + port);
