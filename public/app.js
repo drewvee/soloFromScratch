22a6a411ca5124b8852b41d7a027a39868d22219
Andrew Vedady <drewvedady@gmail.com>
2016-04-05 18:04:23 -0500

    added form field

--- public/app.js
+++ public/app.js
@@ -2,8 +2,23 @@ angular.module('scratch', [])
 
 
 
-.controller('restoCtrl', function($scope) {
-  $scope.title = "Best restaurants I've been so far... ";
+.controller('restoCtrl', function($scope, Restos) {
+  $scope.title = "Restaurants to visit";
+  
+  $scope.data = {};
+  
+  $scope.getRestos = function () {
+    Restos.getAll()
+      .then(function (link) {
+        $scope.data.restos = restos;
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+    
+    $scope.getRestos();
+  };
+  
   $scope.restos = [
    {
     name: 'Terroni\'s - Toronto',
@@ -26,33 +41,46 @@ angular.module('scratch', [])
     review: 'Yakitori all day every day'
    }
   ]
+})
+
+
+.controller('mainCtrl', function($scope, Restos) {
+  $scope.resto = {};
+  
+  $scope.addResto = function(){
+    Restos.addOne($scope.resto)
+      .then(function(res) {
+        console.log(res);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };   
+})
+
+.factory('Restos', function($http) {
+    
+    var getAll = function() {
+      return $http ({
+        method: 'GET',
+        url: '/api/restos'
+      })
+      .then(function(resp) {
+          return resp.data;
+        });
+    };   
+     
+    var addOne = function(resto) {
+      return $http({
+        method: 'POST',
+        url: '/api/restos',
+        data: resto
+        });
+    };
+      
+
+    return {
+      getAll: getAll,
+      addOne: addOne
+    }
 });
-
-
-// .controller('mainCtrl', function($scope, $http) {
-//   $scope.formData = {};
-//   $scope.restos = [];
-  
-//   $http.get('/api/restos')
-//     .success(function(data) {
-//       $scope.restos = data;
-//       console.log(data);
-//     })
-//     .error(function(data) {
-//       console.log('Error: ' + data);
-//     });
-
-//   $http.createResto = function() {
-//     $http.post('/api/restos', $scope.formData)
-//       .success(function(data) {
-//       $scope.formData = {};   //clear form before creating new one
-//       $scope.restos = data;
-//       console.log(data);
-//     })
-//     .error(function(data) {
-//       console.log('Error: ' + data);
-//     });
-//   };
-  
-  
-// });
